b4567e606394443902cc3d0d7de637357ae1f81f
NTM Studio <microplayinteractive@gmail.com>
2022-11-05 17:35:13 +0100

    VM Only debug.

--- Scripts/debug.js
+++ Scripts/debug.js
@@ -1,3 +1,6 @@
+// Developer mode.
+isDebug = false;
+
 function showError(text, content) {
     const error = document.getElementById("errorContainer");
     const Errortitle = document.getElementById("errortitle");
@@ -17,3 +20,21 @@ function logData(data) {
     const debug = document.getElementById("debug");
     debug.innerHTML = data;
 }
+
+function debugonly() {
+    const urlParams = new URLSearchParams(window.location.search);
+    if (urlParams.get("debug") == "true") {
+        isDebug = true;
+    }
+    // Debug only
+    let debugElements = document.getElementsByTagName("debugonly");
+
+    if (!isDebug) {
+        for (var i = 0; i < debugElements.length; i++) {
+            for (var element of debugElements[i].children) {
+                element.style.display = "none";
+            }
+        }
+    }
+}
+debugonly();
